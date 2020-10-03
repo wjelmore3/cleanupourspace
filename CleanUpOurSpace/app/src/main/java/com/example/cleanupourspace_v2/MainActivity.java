33dad2e43e44e60bbbd8132450a6adaf75835208
Liam Shay <shaylp@mail.uc.edu>
2020-10-03 19:52:19 -0400

    Camera Working Code

---
+++ CleanUpOurSpace/app/src/main/java/com/example/cleanupourspace_v2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cleanupourspace_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button yourButton = (Button) findViewById(R.id.open_activity_button);
+
+        yourButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)   {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
+    }
+}
