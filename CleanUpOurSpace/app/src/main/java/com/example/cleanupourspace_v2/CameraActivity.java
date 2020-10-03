95588b02ad9ab8431a8c0fdde9bb6245ad8c0895
Liam Shay <shaylp@mail.uc.edu>
2020-10-03 19:52:19 -0400

    Camera Working Code

---
+++ CleanUpOurSpace/app/src/main/java/com/example/cleanupourspace_v2/CameraActivity.java
@@ -0,0 +1,107 @@
+package com.example.cleanupourspace_v2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+
+    Button mCaptureBtn;
+    ImageView mImageView;
+
+    Uri image_uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        mImageView = findViewById(R.id.image_view);
+        mCaptureBtn = findViewById(R.id.capture_image_btn);
+
+        //button click
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //if system os is newer than marshmallow, request a runtime permission
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ) {
+                        //permission not enabled, make request
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //show request popup
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {
+                        //permission granted
+                        openCamera();
+                    }
+                } else {
+                    //system os older than marshmallow
+                    openCamera();
+                }
+
+            }
+
+
+        });
+
+
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        camIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(camIntent, IMAGE_CAPTURE_CODE);
+
+    }
+    //handling permissions
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //Active when user hits allow or deny from permission request popup
+        switch(requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission from popup is positive
+                    openCamera();
+                } else {
+                    //permission denied
+                    Toast.makeText(this, "Access DENIED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image is taken
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            //set image captures to ImageView
+            mImageView.setImageURI(image_uri);
+        }
+    }
+}
