f633265eb6ab43e1519700e093cbfca84072d4a5
wjelmore3 <willelmore3@gmail.com>
2020-10-04 01:34:36 -0400

    Added navigation bar

--- CleanUpOurSpace/app/src/main/java/com/example/cleanupourspace_v2/ProfileActivity.java
+++ CleanUpOurSpace/app/src/main/java/com/example/cleanupourspace_v2/ProfileActivity.java
@@ -1,11 +1,15 @@
 package com.example.cleanupourspace_v2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
     private Button button;
@@ -14,32 +18,30 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        //Initialize and Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set Camera Selected
+        bottomNavigationView.setSelectedItemId(R.id.Profile);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener((new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                openHome();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.camera:
+                        startActivity(new Intent(getApplicationContext(), CameraActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.Profile:
+                        return true;
+                }
+                return false;
             }
-        });
-
-        button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openCamera();
-            }
-        });
-    }
-
-    /** Called when the user taps the Camera button */
-    public void openHome() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    /** Called when the user taps the Camera button */
-    public void openCamera() {
-        Intent intent = new Intent(this, CameraActivity.class);
-        startActivity(intent);
+        }));
     }
 }
